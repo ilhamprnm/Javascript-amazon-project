aa1f3b216f0848cb77d4d96770877c0d5edae0dc
MuhammadIlham <ilhampurnama78@gmail.com>
2024-03-11 06:56:20 +0700

    Move addToCart into Cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,23 @@
 export const cart = [] ;
+export function addToCart(productId) {
+  
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem ;
+    }
+  });
+
+  const selectElement = document.querySelector(`.js-quantity-selector-${productId}`);
+  const quantity = Number(selectElement.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantity
+     })  
+    }
+  }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML = '';
@@ -59,37 +59,23 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML ;
 
+
+function updateCart() {
+  let cartQuantity = 0 ;
+
+  cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity ;
+  })
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity ;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item ;
-      }
-    });
-
-    const selectElement = document.querySelector(`.js-quantity-selector-${productId}`);
-    const quantity = Number(selectElement.value);
-
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: quantity
-      })  
-    }
-
-    let cartQuantity = 0 ;
-
-    cart.forEach((item) => {
-        cartQuantity += item.quantity ;
-    })
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity ;
+    addToCart(productId);
+    updateCart();
+   
   })
 });
 
